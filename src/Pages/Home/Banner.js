7d2cea3b6a77b08efd5671e6edd381d64906ab16
Arif6474 <arifulislam64743@gmail.com>
2022-05-17 00:33:13 +0600

    service, testimonial, communicate

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import chair from '../../assets/images/chair.png'
+import PrimaryButton from '../Shared/PrimaryButton';
 
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen py-8 ">
-  <div class="hero-content flex-col lg:flex-row-reverse">
-    <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
-    <div class="bg-[url('https://i.ibb.co/mvNw42d/bg.png')]">
-      <h1 class="text-7xl font-bold">Your New Smile Starts Here</h1>
-      <p class="py-6 text-xl">Welcome to Patient Portal, Your Medical Home on the Web. With Patient Portal, you can connect with your doctor through a convenient, safe and secure </p>
-      <button class="btn btn-primary text-bold text-white bg-gradient-to-r from-secondary to-primary">Get Started</button>
+        <div className="hero min-h-screen py-8 ">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
+    <div className="bg-[url('https://i.ibb.co/mvNw42d/bg.png')]">
+      <h1 className="text-7xl font-bold">Your New Smile Starts Here</h1>
+      <p className="py-6 text-xl">Welcome to Patient Portal, Your Medical Home on the Web. With Patient Portal, you can connect with your doctor through a convenient, safe and secure </p>
+      <PrimaryButton>Get Started</PrimaryButton>
     </div>
   </div>
 </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Banner from './Banner';
+import Comminucate from './Comminucate';
+import Dental from './Dental';
+import Info from './Info';
+import MakeAppointment from './MakeAppointment';
+import Services from './Services';
+import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
-        <div>
+        <div px-12>
         <Banner></Banner>
+        <Info></Info>
+        <Services></Services>
+        <Dental></Dental>
+        <MakeAppointment></MakeAppointment>
+        <Testimonials></Testimonials>
+       <Comminucate></Comminucate>
         </div>
     );
 };
